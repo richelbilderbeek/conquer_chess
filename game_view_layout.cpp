232c2a532bfe96d0c04903e3b38783367ed032b4
richelbilderbeek <richel@richelbilderbeek.nl>
2022-07-14 22:31:00 +0200

    Allow two players

--- game_view_layout.cpp
+++ game_view_layout.cpp
@@ -11,24 +11,51 @@ game_view_layout::game_view_layout(
 ) : m_window_size{window_size}
 {
   // board
-  m_tl_board = screen_coordinat(margin_width, margin_width);
   const int max_board_height = m_window_size.get_y() - margin_width - margin_width;
   const int max_board_width = m_window_size.get_x() - margin_width - margin_width;
   m_board_width = std::min(max_board_height, max_board_width);
   m_board_height = m_board_width;
-  m_br_board = screen_coordinat(margin_width + m_board_width, margin_width + m_board_height);
 
-  // side
   m_panel_height = static_cast<int>(static_cast<double>(window_size.get_y() - (4 * margin_width)) / 3.0);
-  m_panel_width = static_cast<int>(static_cast<double>(window_size.get_x() - m_board_width - (3 * margin_width)));
-  m_tl_units_2 = screen_coordinat(margin_width + m_board_width + margin_width, margin_width);
-  m_tl_controls_2 = m_tl_units_2 + screen_coordinat(0, m_panel_height + margin_width);
-  m_tl_debug_2 = m_tl_controls_2 + screen_coordinat(0, m_panel_height + margin_width);
+  m_panel_width = static_cast<int>(static_cast<double>(window_size.get_x() - m_board_width - (4 * margin_width))) / 2;
+
+  const int x1{margin_width};
+  const int x2{x1 + m_panel_width};
+  const int x3{x2 + margin_width};
+  const int x4{x3 + m_board_width};
+  const int x5{x4 + margin_width};
+  const int x6{x5 + m_panel_width};
+
+  const int y1{margin_width};
+  const int y2{y1 + m_panel_height};
+  const int y3{y2 + margin_width};
+  const int y4{y3 + m_panel_height};
+  const int y5{y4 + margin_width};
+  const int y6{y5 + m_panel_height};
 
 
-  m_br_units_2 = m_tl_units_2 + screen_coordinat(m_panel_width, m_panel_height);
-  m_br_controls_2 = m_tl_controls_2 + screen_coordinat(m_panel_width, m_panel_height);
-  m_br_debug_2 = m_tl_debug_2 + screen_coordinat(m_panel_width, m_panel_height);
+  // Panel 1
+  m_tl_units_1 = screen_coordinat(x1, y1);
+  m_tl_controls_1 = screen_coordinat(x1, y3);
+  m_tl_debug_1 = screen_coordinat(x1, y5);
+
+  m_br_units_1 = screen_coordinat(x2, y2);
+  m_br_controls_1 = screen_coordinat(x2, y4);
+  m_br_debug_1 = screen_coordinat(x2, y6);
+
+  // Board
+  m_tl_board = screen_coordinat(x3, y1);
+  m_br_board = screen_coordinat(x4, y6);
+
+
+  // Panel 2
+  m_tl_units_2 = screen_coordinat(x5, y1);
+  m_tl_controls_2 = screen_coordinat(x5, y3);
+  m_tl_debug_2 = screen_coordinat(x5, y5);
+
+  m_br_units_2 = screen_coordinat(x6, y2);
+  m_br_controls_2 = screen_coordinat(x6, y4);
+  m_br_debug_2 = screen_coordinat(x6, y6);
 }
 
 game_coordinat convert_to_game_coordinat(
@@ -110,15 +137,59 @@ void test_game_view_layout()
   {
     const int margin_width = 10;
     const game_view_layout layout{screen_coordinat(1024, 576), margin_width};
-    assert(layout.get_tl_board().get_x() == 0.0 + margin_width);
-    assert(layout.get_tl_board().get_y() == 0.0 + margin_width);
-    assert(layout.get_br_board().get_x() == 576.0 - margin_width);
-    assert(layout.get_br_board().get_y() == 576.0 - margin_width);
+    const int x1{margin_width};
+    const int x2{x1 + layout.get_panel_width()};
+    const int x3{x2 + margin_width};
+    const int x4{x3 + layout.get_board_width()};
+    const int x5{x4 + margin_width};
+    const int x6{x5 + layout.get_panel_width()};
 
-    assert(layout.get_tl_units_2().get_x() == 576.0 - margin_width + margin_width);
-    assert(layout.get_tl_units_2().get_y() == margin_width);
-    assert(layout.get_br_debug_2().get_x() == 1024 - margin_width);
-    assert(is_close(layout.get_br_debug_2().get_y(), 576 - margin_width, 3));
+    const int y1{margin_width};
+    const int y2{y1 + layout.get_panel_height()};
+    const int y3{y2 + margin_width};
+    const int y4{y3 + layout.get_panel_height()};
+    const int y5{y4 + margin_width};
+    const int y6{y5 + layout.get_panel_height()};
+
+    assert(layout.get_tl_units_1().get_x() == x1);
+    assert(layout.get_tl_units_1().get_y() == y1);
+    assert(layout.get_br_units_1().get_x() == x2);
+    assert(layout.get_br_units_1().get_y() == y2);
+
+    assert(layout.get_tl_controls_1().get_x() == x1);
+    assert(layout.get_tl_controls_1().get_y() == y3);
+    assert(layout.get_br_controls_1().get_x() == x2);
+    assert(layout.get_br_controls_1().get_y() == y4);
+
+    assert(layout.get_tl_debug_1().get_x() == x1);
+    assert(layout.get_tl_debug_1().get_y() == y5);
+    assert(layout.get_br_debug_1().get_x() == x2);
+    assert(layout.get_br_debug_1().get_y() == y6);
+
+    assert(layout.get_tl_board().get_x() == x3);
+    assert(layout.get_tl_board().get_y() == y1);
+    assert(layout.get_br_board().get_x() == x4);
+    assert(layout.get_br_board().get_y() == y6);
+
+
+
+
+
+    assert(layout.get_tl_units_2().get_x() == x5);
+    assert(layout.get_tl_units_2().get_y() == y1);
+    assert(layout.get_br_units_2().get_x() == x6);
+    assert(layout.get_br_units_2().get_y() == y2);
+
+    assert(layout.get_tl_controls_2().get_x() == x5);
+    assert(layout.get_tl_controls_2().get_y() == y3);
+    assert(layout.get_br_controls_2().get_x() == x6);
+    assert(layout.get_br_controls_2().get_y() == y4);
+
+    assert(layout.get_tl_debug_2().get_x() == x5);
+    assert(layout.get_tl_debug_2().get_y() == y5);
+    assert(layout.get_br_debug_2().get_x() == x6);
+    assert(layout.get_br_debug_2().get_y() == y6);
+    /*
     const int board_width{layout.get_board_width()};
     assert(board_width == 576.0 - margin_width - margin_width);
     const int board_height{layout.get_board_height()};
@@ -127,6 +198,7 @@ void test_game_view_layout()
     assert(layout.get_square_width() == square_width);
     const double square_height{board_height / 8.0};
     assert(layout.get_square_height() == square_height);
+    */
   }
   //--------------------------------------------------------------------------
   // game -> screen
@@ -170,7 +242,7 @@ void test_game_view_layout()
     assert(tl_board.get_x() == 0.0);
     assert(tl_board.get_y() == 8.0);
     assert(br_board.get_x() == 8.0);
-    assert(br_board.get_y() == 0.0);
+    assert(is_close(br_board.get_y(), 0.0, 0.03));
   }
   #endif
 }
--- game_view_layout.h
+++ game_view_layout.h
@@ -8,31 +8,44 @@
 constexpr int get_default_margin_width() noexcept { return 10; }
 
 /// The layout of the game view
+///   x          x x             x x          x
+///   1          2 3             4 5          6
 ///
-/// +------------------------------+
-/// |                              |
-/// | 1-------------+ 3----------+ |
-/// | |             | | units    | |
-/// | | board       | +----------4 |
-/// | |             |              |
-/// | |             | 5----------+ |
-/// | |             | | controls | |
-/// | |             | +----------6 |
-/// | |             |              |
-/// | |             | 7----------+ |
-/// | |             | | debug    | |
-/// | +-------------2 +----------8 |
-/// |                              |
-/// +------------------------------+
+/// +-------------------------------------------+
+/// |                                           |
+/// | 3----------+ 1-------------+ 9----------+ | <--+                       y1
+/// | | units    | |             | | units    | |    |
+/// | +----------4 | board       | +----------A |    +-- board_height        y2
+/// |              |             |              |    |
+/// | 5----------+ |             | B----------+ |    | <--+                  y3
+/// | | controls | |             | | controls | |    |    +-- panel_height
+/// | +----------6 |             | +----------C |    | <--+                  y4
+/// |              |             |              |    |
+/// | 7----------+ |             | D----------+ |    |                       y5
+/// | | debug    | |             | | debug    | |    |
+/// | +----------8 +-------------2 +----------E | <--+                       y6
+/// |                                           |
+/// +-------------------------------------------+
+///
+///   ^          ^ ^            ^  ^          ^
+///   |          | |            |  |          |
+///   +----------+ +------------+  +----------+
+///    panel_width  board_width     panel_width
 ///
 /// 1: tl_board
 /// 2: br_board
-/// 3: tl_units
-/// 4: br_units
-/// 5: tl_controls
-/// 6: br_controls
-/// 7: tl_debug
-/// 8: br_debug
+/// 3: tl_units_1
+/// 4: br_units_1
+/// 5: tl_controls_1
+/// 6: br_controls_1
+/// 7: tl_debug_1
+/// 8: br_debug_1
+/// 9: tl_units_2
+/// A: br_units_2
+/// B: tl_controls_2
+/// C: br_controls_2
+/// D: tl_debug_2
+/// E: br_debug_2
 class game_view_layout
 {
 public:
@@ -45,16 +58,25 @@ public:
   int get_board_width() const noexcept { return m_board_width; }
 
   screen_coordinat get_br_board() const noexcept { return m_br_board; }
+  screen_coordinat get_br_controls_1() const noexcept { return m_br_controls_1; }
   screen_coordinat get_br_controls_2() const noexcept { return m_br_controls_2; }
+  screen_coordinat get_br_debug_1() const noexcept { return m_br_debug_1; }
   screen_coordinat get_br_debug_2() const noexcept { return m_br_debug_2; }
+  screen_coordinat get_br_units_1() const noexcept { return m_br_units_1; }
   screen_coordinat get_br_units_2() const noexcept { return m_br_units_2; }
+
+  int get_panel_width() const noexcept { return m_panel_width; }
+  int get_panel_height() const noexcept { return m_panel_height; }
 
   double get_square_height() const noexcept;
   double get_square_width() const noexcept;
 
   screen_coordinat get_tl_board() const noexcept { return m_tl_board; }
+  screen_coordinat get_tl_controls_1() const noexcept { return m_tl_controls_1; }
   screen_coordinat get_tl_controls_2() const noexcept { return m_tl_controls_2; }
+  screen_coordinat get_tl_debug_1() const noexcept { return m_tl_debug_1; }
   screen_coordinat get_tl_debug_2() const noexcept { return m_tl_debug_2; }
+  screen_coordinat get_tl_units_1() const noexcept { return m_tl_units_1; }
   screen_coordinat get_tl_units_2() const noexcept { return m_tl_units_2; }
 
   screen_coordinat get_window_size() const noexcept { return m_window_size; }
@@ -65,16 +87,22 @@ private:
   int m_board_height;
 
   screen_coordinat m_br_board;
+  screen_coordinat m_br_controls_1;
   screen_coordinat m_br_controls_2;
+  screen_coordinat m_br_debug_1;
   screen_coordinat m_br_debug_2;
+  screen_coordinat m_br_units_1;
   screen_coordinat m_br_units_2;
 
   int m_panel_width;
   int m_panel_height;
 
   screen_coordinat m_tl_board;
+  screen_coordinat m_tl_controls_1;
   screen_coordinat m_tl_controls_2;
+  screen_coordinat m_tl_debug_1;
   screen_coordinat m_tl_debug_2;
+  screen_coordinat m_tl_units_1;
   screen_coordinat m_tl_units_2;
 
   /// The size of the window
