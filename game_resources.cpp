4e2827a908b13e6b5d8e5ea0f3ff65053167bf59
richelbilderbeek <richel@richelbilderbeek.nl>
2022-07-15 17:24:52 +0200

    Add sounds

--- game_resources.cpp
+++ game_resources.cpp
@@ -317,6 +317,72 @@ game_resources::game_resources()
       throw std::runtime_error(msg.toStdString());
     }
   }
+  {
+    const QString filename{"faring_into_battle.ogg"};
+    QFile f(":/resources/" + filename);
+    f.copy(filename);
+    if (!m_faring_into_battle_buffer.loadFromFile(filename.toStdString()))
+    {
+      QString msg{"Cannot find sound file '" + filename + "'"};
+      throw std::runtime_error(msg.toStdString());
+    }
+  }
+  {
+    const QString filename{"its_time_to_rock.ogg"};
+    QFile f(":/resources/" + filename);
+    f.copy(filename);
+    if (!m_its_time_to_rock_buffer.loadFromFile(filename.toStdString()))
+    {
+      QString msg{"Cannot find sound file '" + filename + "'"};
+      throw std::runtime_error(msg.toStdString());
+    }
+  }
+  {
+    const QString filename{"jumping_into_battle.ogg"};
+    QFile f(":/resources/" + filename);
+    f.copy(filename);
+    if (!m_jumping_into_battle_buffer.loadFromFile(filename.toStdString()))
+    {
+      QString msg{"Cannot find sound file '" + filename + "'"};
+      throw std::runtime_error(msg.toStdString());
+    }
+  }
+  {
+    const QString filename{"lets_rule.ogg"};
+    QFile f(":/resources/" + filename);
+    f.copy(filename);
+    if (!m_lets_rule_buffer.loadFromFile(filename.toStdString()))
+    {
+      QString msg{"Cannot find sound file '" + filename + "'"};
+      throw std::runtime_error(msg.toStdString());
+    }
+  }
+  {
+    const QString filename{"moving_forward.ogg"};
+    QFile f(":/resources/" + filename);
+    f.copy(filename);
+    if (!m_moving_forward_buffer.loadFromFile(filename.toStdString()))
+    {
+      QString msg{"Cannot find sound file '" + filename + "'"};
+      throw std::runtime_error(msg.toStdString());
+    }
+  }
+  {
+    const QString filename{"to_rule_is_to_act.ogg"};
+    QFile f(":/resources/" + filename);
+    f.copy(filename);
+    if (!m_to_rule_is_to_act_buffer.loadFromFile(filename.toStdString()))
+    {
+      QString msg{"Cannot find sound file '" + filename + "'"};
+      throw std::runtime_error(msg.toStdString());
+    }
+  }
+  m_faring_into_battle.setBuffer(m_faring_into_battle_buffer);
+  m_its_time_to_rock.setBuffer(m_its_time_to_rock_buffer);
+  m_jumping_into_battle.setBuffer(m_jumping_into_battle_buffer);
+  m_lets_rule.setBuffer(m_lets_rule_buffer);
+  m_moving_forward.setBuffer(m_moving_forward_buffer);
+  m_to_rule_is_to_act.setBuffer(m_to_rule_is_to_act_buffer);
 }
 
 sf::Texture& game_resources::get_piece(
--- game_resources.h
+++ game_resources.h
@@ -66,6 +66,20 @@ private:
   sf::Texture m_white_square;
   sf::Font m_font;
   sf::Music m_ninja_gods;
+
+  sf::Sound m_faring_into_battle;
+  sf::Sound m_its_time_to_rock;
+  sf::Sound m_jumping_into_battle;
+  sf::Sound m_lets_rule;
+  sf::Sound m_moving_forward;
+  sf::Sound m_to_rule_is_to_act;
+
+  sf::SoundBuffer m_faring_into_battle_buffer;
+  sf::SoundBuffer m_its_time_to_rock_buffer;
+  sf::SoundBuffer m_jumping_into_battle_buffer;
+  sf::SoundBuffer m_lets_rule_buffer;
+  sf::SoundBuffer m_moving_forward_buffer;
+  sf::SoundBuffer m_to_rule_is_to_act_buffer;
 };
 
 /// Test the game resources
