946bc5b3a801dc0b624cc45f6df8f2470373b55b
richelbilderbeek <richel@richelbilderbeek.nl>
2022-07-21 19:33:50 +0200

    Load all resources

--- sound_effects.cpp
+++ sound_effects.cpp
@@ -9,13 +9,35 @@ sound_effects::sound_effects()
 {
   const std::vector<std::tuple<std::reference_wrapper<sf::Sound>, std::reference_wrapper<sf::SoundBuffer>, std::string>> v = {
     std::make_tuple(std::ref(m_faring_into_battle), std::ref(m_faring_into_battle_buffer), "faring_into_battle.ogg"),
+    std::make_tuple(std::ref(m_heu_high), std::ref(m_heu_high_buffer), "heu_high.ogg"),
+    std::make_tuple(std::ref(m_heu_low), std::ref(m_heu_low_buffer), "heu_low.ogg"),
+    std::make_tuple(std::ref(m_heu_mid), std::ref(m_heu_mid_buffer), "heu_mid.ogg"),
     std::make_tuple(std::ref(m_hide), std::ref(m_hide_buffer), "hide.ogg"),
+    std::make_tuple(std::ref(m_hmm_high), std::ref(m_hmm_high_buffer), "hmm_high.ogg"),
+    std::make_tuple(std::ref(m_hmm_low), std::ref(m_hmm_low_buffer), "hmm_low.ogg"),
+    std::make_tuple(std::ref(m_hmm_mid), std::ref(m_hmm_mid_buffer), "hmm_mid.ogg"),
+    std::make_tuple(std::ref(m_i_cannot_high), std::ref(m_i_cannot_high_buffer), "i_cannot_high.ogg"),
+    std::make_tuple(std::ref(m_i_cannot_low), std::ref(m_i_cannot_low_buffer), "i_cannot_low.ogg"),
+    std::make_tuple(std::ref(m_i_cannot_mid), std::ref(m_i_cannot_mid_buffer), "i_cannot_mid.ogg"),
+    std::make_tuple(std::ref(m_i_cant_high), std::ref(m_i_cant_high_buffer), "i_cant_high.ogg"),
+    std::make_tuple(std::ref(m_i_cant_low), std::ref(m_i_cant_low_buffer), "i_cant_low.ogg"),
+    std::make_tuple(std::ref(m_i_cant_mid), std::ref(m_i_cant_mid_buffer), "i_cant_mid.ogg"),
     std::make_tuple(std::ref(m_its_time_to_rock), std::ref(m_its_time_to_rock_buffer), "its_time_to_rock.ogg"),
     std::make_tuple(std::ref(m_jumping_into_battle), std::ref(m_jumping_into_battle_buffer), "jumping_into_battle.ogg"),
     std::make_tuple(std::ref(m_lets_rule), std::ref(m_lets_rule_buffer), "lets_rule.ogg"),
     std::make_tuple(std::ref(m_moving_forward), std::ref(m_moving_forward_buffer), "moving_forward.ogg"),
-    std::make_tuple(std::ref(m_to_rule_is_to_act), std::ref(m_to_rule_is_to_act_buffer), "to_rule_is_to_act.ogg")
+    std::make_tuple(std::ref(m_no_high), std::ref(m_no_high_buffer), "no_high.ogg"),
+    std::make_tuple(std::ref(m_no_low), std::ref(m_no_low_buffer), "no_low.ogg"),
+    std::make_tuple(std::ref(m_no_mid), std::ref(m_no_mid_buffer), "no_mid.ogg"),
+    std::make_tuple(std::ref(m_nope_high), std::ref(m_nope_high_buffer), "nope_high.ogg"),
+    std::make_tuple(std::ref(m_nope_low), std::ref(m_nope_low_buffer), "nope_low.ogg"),
+    std::make_tuple(std::ref(m_nope_mid), std::ref(m_nope_mid_buffer), "nope_mid.ogg"),
+    std::make_tuple(std::ref(m_to_rule_is_to_act), std::ref(m_to_rule_is_to_act_buffer), "to_rule_is_to_act.ogg"),
+    std::make_tuple(std::ref(m_yes_high), std::ref(m_yes_high_buffer), "yes_high.ogg"),
+    std::make_tuple(std::ref(m_yes_low), std::ref(m_yes_low_buffer), "yes_low.ogg"),
+    std::make_tuple(std::ref(m_yes_mid), std::ref(m_yes_mid_buffer), "yes_mid.ogg"),
   };
+
   for (const auto& p: v)
   {
     const QString filename{std::get<2>(p).c_str()};
--- sound_effects.h
+++ sound_effects.h
@@ -52,12 +52,33 @@ private:
   sf::Sound m_yes_mid;
 
   sf::SoundBuffer m_faring_into_battle_buffer;
+  sf::SoundBuffer m_heu_high_buffer;
+  sf::SoundBuffer m_heu_low_buffer;
+  sf::SoundBuffer m_heu_mid_buffer;
   sf::SoundBuffer m_hide_buffer;
+  sf::SoundBuffer m_hmm_high_buffer;
+  sf::SoundBuffer m_hmm_low_buffer;
+  sf::SoundBuffer m_hmm_mid_buffer;
+  sf::SoundBuffer m_i_cannot_high_buffer;
+  sf::SoundBuffer m_i_cannot_low_buffer;
+  sf::SoundBuffer m_i_cannot_mid_buffer;
+  sf::SoundBuffer m_i_cant_high_buffer;
+  sf::SoundBuffer m_i_cant_low_buffer;
+  sf::SoundBuffer m_i_cant_mid_buffer;
   sf::SoundBuffer m_its_time_to_rock_buffer;
   sf::SoundBuffer m_jumping_into_battle_buffer;
   sf::SoundBuffer m_lets_rule_buffer;
   sf::SoundBuffer m_moving_forward_buffer;
+  sf::SoundBuffer m_no_high_buffer;
+  sf::SoundBuffer m_no_low_buffer;
+  sf::SoundBuffer m_no_mid_buffer;
+  sf::SoundBuffer m_nope_high_buffer;
+  sf::SoundBuffer m_nope_low_buffer;
+  sf::SoundBuffer m_nope_mid_buffer;
   sf::SoundBuffer m_to_rule_is_to_act_buffer;
+  sf::SoundBuffer m_yes_high_buffer;
+  sf::SoundBuffer m_yes_low_buffer;
+  sf::SoundBuffer m_yes_mid_buffer;
 };
 
 #endif // LOGIC_ONLY
