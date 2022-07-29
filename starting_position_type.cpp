46ec5ee2c1d9f9f4a59ba86bbd666f34aef2a4e1
richelbilderbeek <richel@richelbilderbeek.nl>
2022-07-29 07:02:55 +0200

    Fix bug in starting position type, get_next

--- starting_position_type.cpp
+++ starting_position_type.cpp
@@ -19,6 +19,7 @@ starting_position_type get_next(const starting_position_type starting_position) 
 {
   const auto v{get_all_starting_position_types()};
   auto there{std::find(std::begin(v), std::end(v), starting_position)};
+  assert(there != std::end(v));
   if (there == std::end(v) - 1)
   {
     assert(!v.empty());
