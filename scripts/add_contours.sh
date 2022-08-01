587a94ee8738f50c43e79891aedf0e21979cbed9
richelbilderbeek <richel@richelbilderbeek.nl>
2022-08-01 12:27:07 +0200

    White pieces have contours

--- scripts/add_contours.sh
+++ scripts/add_contours.sh
@@ -11,3 +11,10 @@ convert resources/nb.png -bordercolor none -border 10x10 -background white -alph
 convert resources/pb.png -bordercolor none -border 10x10 -background white -alpha background -channel A -blur 0x10 -level 0,70% resources/pb_with_contour.png
 convert resources/qb.png -bordercolor none -border 10x10 -background white -alpha background -channel A -blur 0x10 -level 0,70% resources/qb_with_contour.png
 convert resources/rb.png -bordercolor none -border 10x10 -background white -alpha background -channel A -blur 0x10 -level 0,70% resources/rb_with_contour.png
+
+convert resources/bw.png -bordercolor none -border 10x10 -background black -alpha background -channel A -blur 0x10 -level 0,70% resources/bw_with_contour.png
+convert resources/kw.png -bordercolor none -border 10x10 -background black -alpha background -channel A -blur 0x10 -level 0,70% resources/kw_with_contour.png
+convert resources/nw.png -bordercolor none -border 10x10 -background black -alpha background -channel A -blur 0x10 -level 0,70% resources/nw_with_contour.png
+convert resources/pw.png -bordercolor none -border 10x10 -background black -alpha background -channel A -blur 0x10 -level 0,70% resources/pw_with_contour.png
+convert resources/qw.png -bordercolor none -border 10x10 -background black -alpha background -channel A -blur 0x10 -level 0,70% resources/qw_with_contour.png
+convert resources/rw.png -bordercolor none -border 10x10 -background black -alpha background -channel A -blur 0x10 -level 0,70% resources/rw_with_contour.png
