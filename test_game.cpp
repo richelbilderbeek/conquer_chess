b1f3ee29451edb37dd470d9d0db30001f924c2ef
richelbilderbeek <richel@richelbilderbeek.nl>
2022-08-05 20:15:29 +0200

    Activate bigger tests again

--- test_game.cpp
+++ test_game.cpp
@@ -326,7 +326,6 @@ void test_game_keyboard_use()
     g.tick();
     assert(count_selected_units(g, chess_color::white) == 1);
   }
-  #ifdef FIX_PAWN_MOVES_FORWARD
   // Keyboard: can move pawn forward
   {
     game g;
@@ -348,8 +347,6 @@ void test_game_keyboard_use()
     const auto message{messages.at(1)};
     assert(message.get_message_type() == message_type::start_move);
   }
-  #endif
-  #ifdef FIX_PAWN_MOVES_FORWARD_AGAIN
   // Keyboard: cannot move pawn backward
   {
     game g;
@@ -366,7 +363,6 @@ void test_game_keyboard_use()
     assert(get_closest_piece_to(g, to_coordinat("e2")).get_type() == piece_type::pawn);
     assert(collect_messages(g).at(1).get_message_type() == message_type::cannot);
   }
-  #endif
 #endif // NDEBUG // no tests in release
 }
 
@@ -435,8 +431,6 @@ void test_game() //!OCLINT tests may be many
   test_game_functions();
   test_game_keyboard_use();
   test_game_mouse_use();
-#ifdef FIX_READY_FOR_SCENARIOS
   test_game_scenarios();
-#endif
 #endif // no tests in release
 }
