37c2ba4d75ceab75d680cddf1af46afe3f225420
richelbilderbeek <richel@richelbilderbeek.nl>
2023-01-08 17:23:51 +0100

    Moved fonts to own folder, progress #94

--- fonts.cpp
+++ fonts.cpp
@@ -7,7 +7,7 @@ fonts::fonts()
   // Load font file
   {
     const QString filename{"arial.ttf"};
-    QFile f(":/resources/" + filename);
+    QFile f(":/resources/fonts/" + filename);
     f.copy(filename);
     if (!m_arial_font.loadFromFile(filename.toStdString()))
     {
@@ -18,7 +18,7 @@ fonts::fonts()
   // Load font file
   {
     const QString filename{"CodeSquaredRegular-AYRg.ttf"};
-    QFile f(":/resources/" + filename);
+    QFile f(":/resources/fonts/" + filename);
     f.copy(filename);
     if (!m_code_squared_font.loadFromFile(filename.toStdString()))
     {
@@ -29,7 +29,7 @@ fonts::fonts()
   // Load font file
   {
     const QString filename{"16114_FuturistFixed-width.ttf"};
-    QFile f(":/resources/" + filename);
+    QFile f(":/resources/fonts/" + filename);
     f.copy(filename);
     if (!m_futuristic_font.loadFromFile(filename.toStdString()))
     {
