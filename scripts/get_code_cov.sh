41bc64dc6877018b4f96ba29f24abd9830757645
richelbilderbeek <richel@richelbilderbeek.nl>
2023-11-02 09:20:14 +0100

    Fix get_code_cov script style

--- scripts/get_code_cov.sh
+++ scripts/get_code_cov.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cp debug/*.gcda .
-cp debug/*.gcno .
-
-for filename in $(find . | egrep '\.cpp$');
+for filename in $(find . | grep -E '\.cpp$')
 do 
-  gcov -n -o . $filename > /dev/null; 
+  gcov -n -o . "${filename}" > /dev/null
 done
