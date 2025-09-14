94b83fad235e671aba8c201d8518cd1fb3f89a53
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-14 09:47:56 +0200

    OK

--- scripts/get_code_cov.sh
+++ scripts/get_code_cov.sh
@@ -6,5 +6,7 @@ cp debug/*.gcno .
 for filename in $(find . | grep -E '\.cpp$');
 do 
   echo "Filename: ${filename}"
-  gcov -n -o . "${filename}" > /dev/null; 
+  gcov -n -o . "${filename}" > /dev/null
 done
+
+exit 0
