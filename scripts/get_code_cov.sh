0698c84694dd85a8bff95f17bbc2785bfe0ee5e9
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-14 10:11:21 +0200

    More verbose script

--- scripts/get_code_cov.sh
+++ scripts/get_code_cov.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+echo "Copying gcov files to root folder"
 cp debug/*.gcda .
 cp debug/*.gcno .
 
+echo "Extracting coverage info from the gcov files"
 for filename in $(find . | grep -E '\.cpp$');
 do 
   echo "Filename: ${filename}"
   gcov -n -o . "${filename}" > /dev/null
 done
 
+echo "Succes: done extracting code coverage information"
 exit 0
